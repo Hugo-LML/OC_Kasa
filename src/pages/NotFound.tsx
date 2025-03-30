90af1cdcc2738c43c1924a3379019c630e75b242
Hugo Lemerle <hlml200294@gmail.com>
2025-03-30 11:56:57 +0200

    feat(404): not found page added

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,16 @@
 import { FC } from "react";
+import Layout from "../components/layout/Layout";
+import { Link } from "react-router-dom";
 
 const NotFound: FC = () => {
   return (
-    <div>
-      <h1>ERROR 404</h1>
-    </div>
+    <Layout id="not-found">
+      <h1>404</h1>
+      <h2>Oups ! La page que vous demandez n'existe pas</h2>
+      <Link to="/">
+        Retourner sur la page d'accueil
+      </Link>
+    </Layout>
   );
 };
 
